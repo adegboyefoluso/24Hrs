fb075ad3c952810aa3d8d0bae3647c9e0ec90cce
Kristen <kristengamelin@gmail.com>
2021-09-07 16:30:52 -0400

    added comment class

--- 24Hrs.Data/Comment.cs
+++ 24Hrs.Data/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace _24Hrs.Data
 {
     public class Comment
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public string text { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Required]
+        public DateTimeOffset ModifiedUtc { get; set; }
+        [Required]
+        public virtual Post Post { get; set; }
     }
 }
