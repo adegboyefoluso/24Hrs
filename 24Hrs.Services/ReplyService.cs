809c18a54bf0ca2537820ecd837b6ef74f262a59
Foluso Adegboye <foluso.o.adegboye@gmail.com>
2021-09-07 19:21:18 -0400

    Reply Services Completed

--- 24Hrs.Services/ReplyService.cs
+++ 24Hrs.Services/ReplyService.cs
@@ -31,6 +31,7 @@ namespace _24Hrs.Services
                         Text = model.Text,
                         CommentId = model.CommentId,
                         CreatedUTc = DateTime.Now,
+                        AuthorId=_UserId
 
                     };
                     ctx.Replies.Add(entity);
@@ -39,8 +40,85 @@ namespace _24Hrs.Services
                 return false;
             }
         }
-       
 
+       public ReplyCommentDetail GetReplyByCommentId(int id)
+       {
+            using(var ctx= new ApplicationDbContext())
+            {
+                var query = ctx
+                                .Comments
+                               .SingleOrDefault(e => e.Id == id);
+
+                return new ReplyCommentDetail()
+                {
+                    CommentId = query.Id,
+                    CreatedUtc = query.CreatedUtc,
+                    ModifiedUtc = query.ModifiedUtc,
+                    Text = query.Text,
+                    Replies = ctx
+                                    .Replies
+                                    .Where(e => e.CommentId == id)
+                                    .Select(e => new ReplyDetail()
+                                    {
+                                        ReplyId = e.Id,
+                                        Text = e.Text,
+                                        CreatedUTc = e.CreatedUTc,
+                                        ModifiedUTc = e.ModifiedUTc
+                                    }).ToList()
+
+
+                };
+                
+            }
+        }
+
+        public List<ReplyDetail> GetReplyByAuthorId()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                                    .Replies
+                                    .Where(e => e.AuthorId == _UserId)
+                                    .Select(e => new ReplyDetail()
+                                    {
+                                        ReplyId = e.Id,
+                                        Text = e.Text,
+                                        CreatedUTc = e.CreatedUTc,
+                                        ModifiedUTc = e.ModifiedUTc
+                                    }).ToList();
+
+                return query;
+            }
+        }
+
+        public bool EditReply (ReplyEdit model)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var reply = ctx
+                                .Replies
+                                .SingleOrDefault(e => e.Id == model.Id & e.AuthorId==_UserId);
+                if (reply is null) return false;
+                reply.Text = model.Text;
+                reply.CommentId = model.CommentId;
+                reply.ModifiedUTc = DateTimeOffset.Now;
+
+                return ctx.SaveChanges() == 1;
+                                
+            }
+        }
+        public bool DeleteReply (int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var reply = ctx
+                               .Replies
+                               .SingleOrDefault(e => e.Id ==id&&e.AuthorId==_UserId);
+                if (reply is null) return false;
+                ctx.Replies.Remove(reply);
+                return ctx.SaveChanges() == 1;
+            }
+        }
        
     }
 }
