9bcdbcff00609f13401025f8f4574f0c80e0808c
Foluso Adegboye <foluso.o.adegboye@gmail.com>
2021-09-07 15:00:20 -0400

    Commit Changes made to Identity Model

--- 24Hrs.Data/IdentityModels.cs
+++ 24Hrs.Data/IdentityModels.cs
@@ -34,9 +34,10 @@ namespace _24HrsProject.Data
             return new ApplicationDbContext();
         }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<Reply> Replies { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
         public DbSet<Like> Likes { get; set; }
+        
 
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
--- 24Hrs.Data/Reply.cs
+++ 24Hrs.Data/Reply.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _24Hrs.Data
 {
-    class Reply
+    public class Reply
     {
     }
 }
