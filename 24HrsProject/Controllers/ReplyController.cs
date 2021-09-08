f1d9053f466ec6fbc3fa65c5b0164433f8b14ac2
Foluso Adegboye <foluso.o.adegboye@gmail.com>
2021-09-07 20:37:11 -0400

    Reply controller Added

---
+++ 24HrsProject/Controllers/ReplyController.cs
@@ -0,0 +1,85 @@
+﻿using _24Hrs.Model.Reply;
+using _24Hrs.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HrsProject.Controllers
+{
+    [Authorize]
+    public class ReplyController : ApiController
+    {
+        private ReplyService CreateReplyServices()
+        {
+            var userid = Guid.Parse(User.Identity.GetUserId());
+            var service = new ReplyService(userid);
+            return service;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(ReplyCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreateReplyServices();
+            var reply=service.CreateRepy(model);
+            if (reply)
+            {
+                return Ok("Reply Created succesfuly");
+            }
+            return InternalServerError();
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetReplyByIdCommentId(int id)
+        {
+            var service = CreateReplyServices();
+           var reply= service.GetReplyByCommentId(id);
+            return Ok(reply);
+            
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult GetReplyByAuthor()
+        {
+            var service = CreateReplyServices();
+            var reply = service.GetReplyByAuthorId();
+            return Ok(reply);
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteReply(int id)
+        {
+            var service = CreateReplyServices();
+            var reply = service.DeleteReply(id);
+            if (reply is false) return InternalServerError();
+
+            return Ok("Reply deleted succesfully");
+
+        }
+        [HttpPut]
+        public IHttpActionResult Editreply(ReplyEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreateReplyServices();
+            var reply = service.EditReply(model);
+            if (reply is true) return Ok("reply Updated succesfully");
+            return InternalServerError();
+
+        }
+
+    }
+}
